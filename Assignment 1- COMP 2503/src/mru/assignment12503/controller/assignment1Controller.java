dcd102a660b354a2e815d36984b0f300e58dc13b
josesanchez2004 <jsanc618@mtroyal.ca>
2022-09-25 23:48:13 -0600

    almost done calculate deductions

--- Assignment 1- COMP 2503/src/mru/assignment12503/controller/assignment1Controller.java
+++ Assignment 1- COMP 2503/src/mru/assignment12503/controller/assignment1Controller.java
@@ -12,19 +12,34 @@ public class assignment1Controller {
 		in = new Scanner(System.in);
 	}
 	
-	public void calculateGrossWeeklyPay(double hoursWorked) {
-		
+	public double calculateGrossWeeklyPay(double hoursWorked,Employee e) {
+		char sType='S';
+		char hType='H';
+		double grossWeeklyPay;
+		char employeeType=e.getType();
+		if(employeeType==sType) {
+			grossWeeklyPay=salaryEmployee(e, hoursWorked);
+		}else if (employeeType==hType) {
+			grossWeeklyPay=hourlyEmployee(e, hoursWorked);
+			
+		}else {
+			grossWeeklyPay=consultantEmployee(e, hoursWorked);
+		}
+		return grossWeeklyPay;
 	}
 	
-	public double salaryEmployee(Employee e) {
-		System.out.println("+++++++++++++++++++++++++++++++++++++++");
-		System.out.println("How many hours did you work this week: ");
-		System.out.println("+++++++++++++++++++++++++++++++++++++++");		
-		double hours = in.nextDouble();
+	//System.out.println("+++++++++++++++++++++++++++++++++++++++");
+	//System.out.println("How many hours did you work this week: ");
+	//System.out.println("+++++++++++++++++++++++++++++++++++++++");
+	//double hours = in.nextDouble();
+	
+	public double salaryEmployee(Employee e, double hour) {
+	
+		double hours = hour;
 		
 		double numOfWeeks = 52;
 		double maxHours = e.getMaxHours();
-		double weeklyPay= e.getPayRate()/52;
+		double weeklyPay= e.getPayRate()/numOfWeeks;
 		
 		//no overtime 
 		if(hours>maxHours) {
@@ -36,15 +51,12 @@ public class assignment1Controller {
 		
 	}
 	
-	public double hourlyEmployee(Employee e) {
+	public double hourlyEmployee(Employee e, double hour) {
 		
-		int maxHours = 60;
+		double maxHours = e.getMaxHours();
 		int fortyHourWeek = 40;
 		double hourlyPay = e.getPayRate();
-		System.out.println("+++++++++++++++++++++++++++++++++++++++");
-		System.out.println("How many hours did you work this week: ");
-		System.out.println("+++++++++++++++++++++++++++++++++++++++");
-		double hours = in.nextDouble(); 
+		double hours = hour;
 		double overtimePay;
 		double total;
 		
@@ -67,12 +79,9 @@ public class assignment1Controller {
 		return total;
 	}
 	
-	public double consultantEmployee(Employee e) {
+	public double consultantEmployee(Employee e,double hour) {
 		double maxHours = e.getMaxHours();
-		System.out.println("+++++++++++++++++++++++++++++++++++++++");
-		System.out.println("How many hours did you work this week: ");
-		System.out.println("+++++++++++++++++++++++++++++++++++++++");
-		double hours = in.nextDouble();
+		double hours = hour;
 		double hourlyPay = e.getPayRate();
 		double total;
 		
@@ -86,5 +95,41 @@ public class assignment1Controller {
 		return total;
 	}
 	
+	public double calcWithhold(double grossWeeklyPay) {
+		
+		double oneThousand=1000;
+		double twoThousand=2000;
+		double normalTax;
+		if (grossWeeklyPay>twoThousand) {
+			double tax=0.17;
+			normalTax= grossWeeklyPay*tax;
+			
+		}else if (grossWeeklyPay<twoThousand && grossWeeklyPay>oneThousand) {
+			
+			double tax=0.12;
+			normalTax=grossWeeklyPay*tax;
+		}
+		else {
+			double tax=0.075;
+			normalTax=grossWeeklyPay*tax;
+			
+		}
+		return normalTax;
+	}	
 	
+	public double calcCPP(double grossWeeklyPay) {
+		double cpp=0.0475;
+		double cppTax=grossWeeklyPay*cpp;
+		return cppTax;
+		
+	}
+	public double calcEI(double grossWeeklyPay) {
+		double ei=0.018;
+		double eiTax=grossWeeklyPay*ei;
+		return eiTax;
+	}
 }
+
+
+
+
